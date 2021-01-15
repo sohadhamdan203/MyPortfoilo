466bc7d91d8abb03be7053c04e8529339a47201d
DESKTOP-UECHB2U\HP <hajjajsohad@gmail.com>
2021-01-15 19:13:03 +0200

    Add project files.

---
+++ Core/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Category :EntityBase
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
